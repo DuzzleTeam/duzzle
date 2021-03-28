12763289e6031ed3ec401ba40327a9cd7c82138b
Juhyun <s2019w09@e-mirim.hs.kr>
2021-03-29 00:51:44 +0900

    Add User model

--- server/index.js
+++ server/index.js
@@ -1,6 +1,20 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const port = 5000;
+
+mongoose
+  .connect(
+    "mongodb+srv://duzzle:duzzle2021@duzzle.55imn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   res.status(200);
