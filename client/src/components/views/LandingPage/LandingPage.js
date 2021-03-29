7f99045a04d625add31df986359f2d71fc586def
cho-hadam <s2019w16@e-mirim.hs.kr>
2021-03-29 21:59:29 +0900

    Landing Page 구조 잡기

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -4,7 +4,29 @@ import "../../../utils/Common.css";
 import "./Sections/LandingPage.css";
 
 function LandingPage() {
-  return <div id="Container">LandingPage</div>;
+  return (
+    <div id="Container">
+      <main>
+        <article>
+          <h1 className="MainText">
+            <span className="text1">더 즐겁게,</span>
+            <span className="text2">DEVELOP 하라</span>
+          </h1>
+        </article>
+
+        <article className="SNSContainer">
+          <div className="Instagram">
+            <span className="InstaText">instagram</span>
+            <img src="/images/arrow.png" alt="instagram" />
+          </div>
+          <div className="Facebook">
+            <span className="FacebookText">facebook</span>
+            <img src="/images/arrow.png" alt="facebook" />
+          </div>
+        </article>
+      </main>
+    </div>
+  );
 }
 
 export default LandingPage;
