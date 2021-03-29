fb5e159b7254a943c31c1433fd0fd38495389e7b
cho-hadam <s2019w16@e-mirim.hs.kr>
2021-03-29 21:07:25 +0900

    NavBar 디자인 구현

--- client/src/components/views/NavBar/NavBar.js
+++ client/src/components/views/NavBar/NavBar.js
@@ -5,14 +5,20 @@ import "./Sections/NavBar.css";
 
 function NavBar() {
   return (
-    <div className="NavContainer">
-      <div id="LogoContainer">
-        <img src="http://localhost:3000/logo.png" alt="Duzzle Logo" />
+    // Nav 최상위 컨테이너
+    <div id="NavContainer">
+      {/* Logo */}
+      <div className="NavLeftContainer">
+        <Link to="/">
+          <img src="http://localhost:3000/logo.png" alt="Duzzle Logo" />
+        </Link>
+        {/* Menu */}
+        <ul className="NavList">
+          <Link to="wezzle">위즐</Link>
+          <Link to="mezzle">미즐</Link>
+        </ul>
       </div>
-      <ul className="NavList">
-        <Link to="wezzle">위즐</Link>
-        <Link to="mezzle">미즐</Link>
-      </ul>
+      {/* 로그인 or 프로필 사진 & 알림 */}
       <div id="AuthMenu">
         <Link to="login">로그인</Link>
       </div>
