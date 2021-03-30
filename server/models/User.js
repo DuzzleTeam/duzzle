e850bc51304100a97cf2f21c0b781bb70f9836f7
Juhyun <s2019w09@e-mirim.hs.kr>
2021-03-31 03:19:00 +0900

    Add authentication

--- server/models/User.js
+++ server/models/User.js
@@ -82,6 +82,17 @@ userSchema.methods.generateToken = function (cb) {
   });
 };
 
+// 03.30 / 토큰으로 유저 찾기
+userSchema.statics.findByToken = function (token, cb) {
+  var user = this;
+  jwt.verify(token, "secretToken", function (err, decoded) {
+    user.findOne({ _id: decoded, token: token }, function (err, user) {
+      if (err) return cb(err);
+      cb(null, user);
+    });
+  });
+};
+
 const User = mongoose.model("User", userSchema);
 
 module.exports = { User };
