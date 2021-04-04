4f5e96fb95ce7c50907d216b835b88323358f36d
dayeon <dayeon2399@naver.com>
2021-04-04 15:33:03 +0900

    Login page 버튼 활성화

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,9 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import "../../../utils/Common.css";
 import "./Sections/LoginPage.css";
 
 function LoginPage() {
+	const [inputs, setInputs] = useState({
+		email: "",
+		password: ""
+	});
+	const [isActive, setIsActive] = useState(false);
+
+	const { email, password } = inputs;
+
+	const onChange = e => {
+		const { value, name } = e.target;
+		setInputs({
+			...inputs,
+			[name]: value
+		});
+	};
+
+	useEffect(() => {
+		if (
+			String(email).includes("@e-mirim.hs.kr") &&
+			String(password).length >= 8
+		) {
+			setIsActive(true);
+		} else {
+			setIsActive(false);
+		}
+	}, [inputs]);
+
+	// const onChange = e => {
+	// 	const { value, name } = e.target;
+	// 	console.log(
+	// 		isActive,
+	// 		String([email]).includes("@e-mirim.hs.kr"),
+	// 		String([password]).length
+	// 	);
+	// 	if (
+	// 		String([email]).includes("@e-mirim.hs.kr") &&
+	// 		String([password]).length >= 8
+	// 	) {
+	// 		setInputs({
+	// 			...inputs,
+	// 			[name]: value,
+	// 			isActive: true
+	// 		});
+	// 	} else {
+	// 		setInputs({
+	// 			...inputs,
+	// 			[name]: value,
+	// 			isActive: false
+	// 		});
+	// 	}
+	// };
+
 	return (
 		<div id="Container">
 			{/* Login Page Contents */}
@@ -14,31 +66,43 @@ function LoginPage() {
 				</article>
 
 				{/* input (이메일, 비밀번호, 제출버튼) */}
-				<article className="InputContainer">
-					{/* @e-mirim.hs.kr인 이메일 */}
-					<div>
-						<input
-							className="InputEmail"
-							type="email"
-							placeholder="   e-mail"
-							pattern=".+@e-mirim.hs.kr"
-						/>
-					</div>
+				<article className="LoginContainer">
+					<form>
+						{/* @e-mirim.hs.kr인 이메일 */}
+						<div>
+							<input
+								className="InputEmail"
+								type="email"
+								placeholder="e-mail"
+								name="email"
+								value={email}
+								onChange={onChange}
+							/>
+						</div>
 
-					{/* 8자리 이상인 비밀번호 */}
-					<div>
-						<input
-							className="InputPassword"
-							type="password"
-							placeholder="   PASSWORD"
-							pattern=".{8,}"
-						/>
-					</div>
+						{/* 8자리 이상인 비밀번호 */}
+						<div>
+							<input
+								className="InputPassword"
+								type="password"
+								placeholder="PASSWORD"
+								name="password"
+								value={password}
+								onChange={onChange}
+							/>
+						</div>
 
-					{/* 제출 */}
-					<div>
-						<input className="InputSubmit" type="submit" value="LOGIN" />
-					</div>
+						{/* 제출 */}
+						<div>
+							<button
+								className="InputSubmit"
+								type="submit"
+								disabled={Boolean(isActive) ? false : true}
+							>
+								LOGIN
+							</button>
+						</div>
+					</form>
 				</article>
 			</main>
 		</div>
