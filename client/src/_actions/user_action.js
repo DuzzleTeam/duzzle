c0ad6c44cfd31c3a8875b3e9a45847a2a6c5c66f
dayeon <dayeon2399@naver.com>
2021-04-07 01:32:35 +0900

    Login 프론트/백 연결(테스트전)

---
+++ client/src/_actions/user_action.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+import { LOGIN_USER } from "./types";
+export function loginUser(dataToSubmit) {
+	const request = axios
+		.post("/login", dataToSubmit)
+		.then(response => response.data);
+
+	return {
+		type: LOGIN_USER,
+		payload: request
+	};
+}
