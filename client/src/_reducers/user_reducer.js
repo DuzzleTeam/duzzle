16203fe08a28d7c5d3d871ab1b78fbbfca54fecf
cho-hadam <s2019w16@e-mirim.hs.kr>
2021-04-09 01:55:55 +0900

    Login 백엔드, 프론트엔드 연결 완료

--- client/src/_reducers/user_reducer.js
+++ client/src/_reducers/user_reducer.js
@@ -1,11 +1,11 @@
 import { LOGIN_USER } from "../_actions/types";
 
-export default function (state = {}, action) {
-	switch (action.type) {
-		case LOGIN_USER:
-			return { ...state, loginSuccess: action.payload };
-			break;
-		default:
-			return state;
-	}
+export default function _(state = {}, action) {
+  switch (action.type) {
+    case LOGIN_USER:
+      return { ...state, loginSuccess: action.payload };
+
+    default:
+      return state;
+  }
 }
