29614540e677b47802c354957fd4d5bad097c12d
cho-hadam <s2019w16@e-mirim.hs.kr>
2021-04-12 23:09:18 +0900

    인증 완료 페이지 구현

--- client/src/components/views/RegisterPage/Authentication/ConfirmRegister.js
+++ client/src/components/views/RegisterPage/Authentication/ConfirmRegister.js
@@ -1,7 +1,11 @@
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useParams } from "react-router";
+import { Link } from "react-router-dom";
 import { confirmRegister } from "../../../../_actions/user_action";
+
+import "../../../../utils/Common.css";
+import "./ConfirmRegister.css";
 
 function ConfirmRegister() {
   const dispatch = useDispatch();
@@ -11,7 +15,19 @@ function ConfirmRegister() {
       console.log(res);
     });
   });
-  return <div>인증되었습니다.</div>;
+
+  return (
+    <div id="Container" className="ConfirmContainer">
+      <img src="/images/confirm.png" alt="confirm" />
+      <h1>더즐의 회원가입이 완료되었습니다.</h1>
+      <span>
+        더즐의 회원이 되신 것을 축하드립니다!
+        <br />
+        개발자, 디자이너와의 활동이 가능합니다.
+      </span>
+      <Link to={`/users/email`}>마이페이지로 이동</Link>
+    </div>
+  );
 }
 
 export default ConfirmRegister;
