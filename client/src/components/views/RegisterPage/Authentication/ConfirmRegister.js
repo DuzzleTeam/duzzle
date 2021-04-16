ffdc371ab2f734e8f2797f7a40ba6d47e8215c12
cho-hadam <s2019w16@e-mirim.hs.kr>
2021-04-16 10:02:28 +0900

    remove useState

--- client/src/components/views/RegisterPage/Authentication/ConfirmRegister.js
+++ client/src/components/views/RegisterPage/Authentication/ConfirmRegister.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useParams, withRouter } from "react-router";
 import { Link } from "react-router-dom";
