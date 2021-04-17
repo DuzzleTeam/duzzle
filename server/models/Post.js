d544182d7f370ffc9d3598af728b6ffb59a66f5e
dayeon <dayeon2399@naver.com>
2021-04-17 20:37:11 +0900

    merge feature/post-models

---
+++ server/models/Post.js
@@ -0,0 +1,50 @@
+const { Schema } = require("mongoose");
+const config = require("../config/key");
+
+const postSchema = mongoose.Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  title: {
+    type: String,
+    required: true,
+    maxlength: 20,
+  },
+  content: {
+    text: {
+      type: String,
+      required: true,
+    },
+    images: [String],
+    files: [String],
+  },
+  likeCount: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  isWezzle: {
+    type: Boolean,
+  },
+  recruit: {
+    period: {
+      type: String,
+    },
+    field: {
+      type: [Number],
+    },
+    peopleNum: {
+      type: Number,
+    },
+  },
+  projectPeriod: {
+    type: String,
+  },
+});
+
+const User = mongoose.model("Post", postSchema);
+module.exports = { Post };
