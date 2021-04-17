572a82f2eba75a88a0094b2bdc6ec1bfd284d1c3
cho-hadam <s2019w16@e-mirim.hs.kr>
2021-04-17 20:06:19 +0900

    Modify Like, Comment Model

--- server/models/Comment.js
+++ server/models/Comment.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const commentSchema = mongoose.Schema({
-  user: [{ type: Schema.Types.ObjectId, ref: "User" }],
-  post: [{ type: Schema.Types.ObjectId, ref: "Post" }],
+  user: { type: Schema.Types.ObjectId, ref: "User" },
+  post: { type: Schema.Types.ObjectId, ref: "Post" },
   text: {
     type: String,
     required: true,
--- server/models/Like.js
+++ server/models/Like.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const likeSchema = mongoose.Schema({
-  post: [{ type: Schema.Types.ObjectId, ref: "Post" }],
-  user: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  post: { type: Schema.Types.ObjectId, ref: "Post" },
+  user: { type: Schema.Types.ObjectId, ref: "User" },
 });
 
 const Like = mongoose.model("Like", likeSchema);
