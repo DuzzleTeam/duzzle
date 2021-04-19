e000b6345831b6b7e76adcfb03035af03de9a967
s2019w09 <s2019w09@e-mirim.hs.kr>
2021-04-19 23:59:25 +0900

    user information

--- client/src/components/views/MyPage/MyPage.js
+++ client/src/components/views/MyPage/MyPage.js
@@ -1,8 +1,9 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { withRouter } from "react-router-dom";
 import "./Sections/MyPage.css";
 
 function MyPage() {
+  // 04.18 / 상단 배너 이미지 랜덤으로 띄우기
   useEffect(() => {
     let num = Math.floor(Math.random() * 3) + 1;
     const banner = document.getElementById("topBanner");
@@ -13,8 +14,38 @@ function MyPage() {
     <div id="Container">
       {/* 상단배너 */}
       <div id="topBanner"></div>
+
       {/* 왼쪽 프로필 */}
-      <div id="profile"></div>
+      <div id="profile">
+        <div class="box">
+          <img class="profile" src="../images/profile-image.jpg" />
+        </div>
+        <div class="userInfo1">
+          <h1>오주영</h1>
+          <font color="gray">UI/UX </font>
+          <p>
+            안녕하십니까. 3학년 뉴미디어디자인과 오주영입니다! 자유롭게
+            오픈채팅으로 연락 부탁드립니다~
+          </p>
+        </div>
+        <hr class="hr" />
+        <div class="userInfo2">
+          <h2 class="level">Lv.3 만렙 디자이너</h2>
+          <div class="progress">
+            <progress value="50" max="100"></progress>
+          </div>
+          <p />
+          <div>
+            <strong>소속</strong> <div class="font">DST </div>
+            <p />
+            <strong>메일</strong> <div class="font">2019d12@e-mirim.hs.kr </div>
+            <p />
+            <strong>오픈채팅</strong> <div class="font">asdfeljsfd.com </div>
+            <p />
+          </div>
+        </div>
+      </div>
+
       {/* 내 게시물 */}
       <div id="myPost"></div>
     </div>
