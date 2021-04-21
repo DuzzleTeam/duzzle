3f576fa8a47808c5d190c25bb11dd238cc9d2afa
dayeon <dayeon2399@naver.com>
2021-04-21 19:52:42 +0900

    위즐/미즐 isWezzle 몽고디비 적용

--- client/src/components/views/PostWritingPage/PostWritingPage.js
+++ client/src/components/views/PostWritingPage/PostWritingPage.js
@@ -27,18 +27,24 @@ function PostWritingPage() {
     event.preventDefault();
     console.log("preventDefault가 실행은 되었음");
 
+    const currentPageMenu = document.location.pathname.match(/wezzle|mezzle/);
+    console.log("currentPageMenu", currentPageMenu);
+    let isWezzle = false;
+    if (currentPageMenu[0] === "wezzle") isWezzle = true;
+
     const body = {
       title: inputContents.title,
       contents: { text: inputContents.contents.text },
+      isWezzle: isWezzle,
     };
     console.log(body, "body도 실행은 되었음");
 
-    const currentPageMenu = document.location.pathname.match(/wezzle|mezzle/);
     axios.post(`/api/${currentPageMenu}/write/new`, body).then((res) => {
       if (res.data.createPostSuccess) {
         setInputContents({
           title: "",
           contents: { text: "" },
+          isWezzle: false,
         });
       } else {
         alert("게시글 작성에 실패하였습니다.");
