707edbcb1934c64634a0ac86adc44cd18d9e9aed
cho-hadam <s2019w16@e-mirim.hs.kr>
2021-04-22 14:23:52 +0900

    삼항연산자 -> ??

--- client/src/components/views/PostPage/Sections/Comment.js
+++ client/src/components/views/PostPage/Sections/Comment.js
@@ -99,64 +99,55 @@ function Comment(props) {
   return stateLoaded ? (
     // 댓글 하나의 컨테이너
     <div className="CommentContainer">
-      {/* 댓글 수정할 때는 안 보임 */}
+      {/* 댓글 유저 프로필 사진 */}
+      <img
+        className="CommentUserProfileImage"
+        src="/images/profile-image.jpg"
+        alt="commentUserProfileImage"
+      />
+
       {updatingComment ? (
-        <></>
-      ) : (
-        <div className="CommentLeftContainer">
-          {/* 댓글 유저 프로필 사진 */}
-          <img
-            className="CommentUserProfileImage"
-            src="/images/profile-image.jpg"
-            alt="commentUserProfileImage"
+        <>
+          {/* 댓글 수정 input */}
+          <textarea
+            className="UpdateCommentInput"
+            rows={Math.floor(updateCommentValue.length / 50)}
+            value={updateCommentValue}
+            onChange={(e) => setUpdateCommentValue(e.target.value)}
           />
-          {/* 댓글 유저, 댓글 게시 날짜, 내용 */}
-          <div className="CommentMainContents">
-            <div>
-              <span className="CommentUser">{"조하닮"}</span>
-              <span className="CommentDate">
-                {comment.createdAt.slice(0, 10)}
-              </span>
-            </div>
-            {/* 댓글 내용 */}
-            <span className="CommentText">{comment.text}</span>
+        </>
+      ) : (
+        // 댓글 유저, 댓글 게시 날짜, 내용
+        <div className="CommentMainContents">
+          <div>
+            <span className="CommentUser">{"조하닮"}</span>
+            <span className="CommentDate">
+              {comment.createdAt.slice(0, 10)}
+            </span>
           </div>
+          {/* 댓글 내용 */}
+          <span className="CommentText">{comment.text}</span>
         </div>
       )}
 
-      {isAuth ? (
+      {isAuth && (
         <div className="CommentAuthContainer">
-          {/* 댓글 수정 시에만 보임 */}
-          {updatingComment ? (
-            <>
-              {/* 댓글 수정 input */}
-              <input
-                type="text"
-                className="UpdateCommentInput"
-                value={updateCommentValue}
-                onChange={(e) => setUpdateCommentValue(e.target.value)}
-              />
-              {/* 댓글 수정 취소 버튼 */}
-              <button
-                className="CancelEditCommentButton"
-                onClick={() => setUpdatingComment(false)}
-              >
-                Cancel
-              </button>
-            </>
-          ) : (
-            <></>
+          {updatingComment && (
+            /* 댓글 수정 취소 버튼 */
+            <button
+              className="CancelEditCommentButton"
+              onClick={() => setUpdatingComment(false)}
+            >
+              Cancel
+            </button>
           )}
-
           {/* 댓글 수정 버튼 */}
           <button onClick={handleUpdateComment} className="EditCommentButton">
             {updatingComment ? "Submit" : "수정"}
           </button>
 
           {/* 댓글 수정할 때는 안 보임 */}
-          {updatingComment ? (
-            <></>
-          ) : (
+          {!updatingComment && (
             // 댓글 삭제 폼
             <form onSubmit={handleDeleteComment} method="post">
               {/* 댓글 삭제 버튼 */}
@@ -170,17 +161,17 @@ function Comment(props) {
             </form>
           )}
         </div>
-      ) : (
-        <></>
       )}
 
       {/* 좋아요 버튼 */}
-      <div className="CommentLikeContainer">
-        <span className="CommentLike">{comment.likeCount}</span>
-        <button>
-          <img src="/images/post_like.png" alt="likeIcon" />
-        </button>
-      </div>
+      {!updatingComment && (
+        <div className="CommentLikeContainer">
+          <span className="CommentLike">{comment.likeCount}</span>
+          <button>
+            <img src="/images/post_like.png" alt="likeIcon" />
+          </button>
+        </div>
+      )}
     </div>
   ) : (
     <></>
