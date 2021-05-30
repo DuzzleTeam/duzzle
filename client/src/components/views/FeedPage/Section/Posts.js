02a6859a0a77185ab951d42dc8aade9b757f025e
cho-hadam <s2019w16@e-mirim.hs.kr>
2021-05-31 00:21:01 +0900

    큰 섹션 파일 분리

---
+++ client/src/components/views/FeedPage/Section/Posts.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// CSS
+import "./Posts.css";
+
+function Posts() {
+  return <section className={"FeedPostsContainer"}>posts</section>;
+}
+
+export default Posts;
---
+++ client/src/components/views/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// CSS
+import "./Section/Footer.css";
+
+function Footer() {
+  return <footer>footer</footer>;
+}
+
+export default Footer;
