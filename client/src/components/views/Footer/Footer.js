7243ef212ed04259e19bb6144f8c5fddc95870f8
cho-hadam <s2019w16@e-mirim.hs.kr>
2021-06-07 02:49:16 +0900

    푸터 CSS

--- client/src/components/views/Footer/Footer.js
+++ client/src/components/views/Footer/Footer.js
@@ -1,50 +1,56 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 // CSS
 import "./Section/Footer.css";
 
 function Footer() {
-  return (
-    <footer>
-      <div className="TopFooter">
-        {/* Duzzle 로고 이미지 */}
-        <div className="FooterLogo"></div>
+  // Redux에서 접속 User 정보 가져오기
+  const user = useSelector((state) => state.user.authPayload);
 
-        {/* SNS, Email, Menu */}
-        {/* SNS, Email */}
-        <div className="TopLeftFooter">
-          <div className="FooterSNS"></div>
-          <span>{"duzzlemanager@gmail.com"}</span>
-        </div>
-        {/* Menu */}
-        <div className="FooterMenu">
-          <span>Menu</span>
-          <div className="FooterMenuLinks">
-            <Link to={"/"}>home</Link>
-            <Link to={"/wezzle"}>wezzle</Link>
-            <Link to={"/mypage"}>mypage</Link>
-            <Link to={"/mezzle"}>mezzle</Link>
+  return (
+    user && (
+      <footer>
+        <div className="TopFooter">
+          {/* Duzzle 로고 이미지 */}
+          <div className="FooterLogo">Duzzle</div>
+
+          {/* SNS, Email, Menu */}
+          {/* SNS, Email */}
+          <div className="TopLeftFooter">
+            <div className="FooterSNS"></div>
+            <span>{"duzzlemanager@gmail.com"}</span>
+          </div>
+          {/* Menu */}
+          <div className="FooterMenu">
+            <span>Menu</span>
+            <div className="FooterMenuLinks">
+              <Link to={"/"}>home</Link>
+              <Link to={"/wezzle"}>wezzle</Link>
+              <Link to={`/users/${user.email}`}>mypage</Link>
+              <Link to={"/mezzle"}>mezzle</Link>
+            </div>
           </div>
         </div>
-      </div>
 
-      {/* divider */}
-      <hr className={"FooterDivider"} />
+        {/* divider */}
+        <hr className={"FooterDivider"} />
 
-      {/* 하단 푸터 (copyright, 개인정보처리방침) */}
-      <div className="BottomFooter">
-        <span>{"COPYRIGHT 2021. Duzzle All rights reserved."}</span>
-        <a
-          href="https://chohadam.tistory.com/3"
-          target="_blank"
-          rel="noreferrer"
-          className="InstaText"
-        >
-          개인정보처리방침
-        </a>
-      </div>
-    </footer>
+        {/* 하단 푸터 (copyright, 개인정보처리방침) */}
+        <div className="BottomFooter">
+          <span>{"COPYRIGHT 2021. Duzzle All rights reserved."}</span>
+          <a
+            href="https://chohadam.tistory.com/3"
+            target="_blank"
+            rel="noreferrer"
+            className="InstaText"
+          >
+            개인정보처리방침
+          </a>
+        </div>
+      </footer>
+    )
   );
 }
 
