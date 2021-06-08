ba815b5cab5a7bff1816c69e6be36df8df142eb1
cho-hadam <s2019w16@e-mirim.hs.kr>
2021-06-08 23:17:30 +0900

    기본 프로필 이미지 설정

---
+++ client/src/utils/getRandomNumber.js
@@ -0,0 +1,12 @@
+// 1, 2, 3, 4, 5 중 랜덤하게 하나의 숫자를 리턴함
+// 기본 이미지를 설정
+const getRandomNumber = () => {
+  const N = 5;
+  // 0 이상 1 미만 난수 * 5 => 0 이상 5 미만
+  // + 1 => 1 이상 6 미만
+  // parseInt => 1 이상 5 이하 정수
+  const number = parseInt(Math.random() * N + 1);
+  return number;
+};
+
+export default getRandomNumber;
