e003a6d233458fc458b895597b1ab475fa9add7e
cho-hadam <s2019w16@e-mirim.hs.kr>
2021-06-20 13:43:02 +0900

    Landing Wezzle, Mezzle Text

---
+++ client/src/components/views/LandingPage/Sections/LandingText.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+// CSS
+import "./LandingText.css";
+
+// Wezzle, Mezzle Description
+// chohadam, 2021-06-20
+
+// icon : title 위 puzzle icon
+// text : {
+//  title1,
+//  description1,
+//  title2,
+//  description2
+// }
+function LandingText({ icon, text }) {
+  const { title1, accent, description1, title2, description2 } = text;
+
+  return (
+    <article className={"LandingTextContainer"}>
+      <img src={`/images/landingPage/${icon}.png`} alt={accent} />
+      <h1 className={"LandingTextTitle"}>{title1}</h1>
+      <p className={"LandingTextDescription"}>
+        <span className={"LandingTextAccent"}>{accent}</span>
+        {description1}
+      </p>
+
+      <h2 className={"LandingTextTitle"}>{title2}</h2>
+      <p className={"LandingTextDescription"}>{description2}</p>
+    </article>
+  );
+}
+
+export default LandingText;
