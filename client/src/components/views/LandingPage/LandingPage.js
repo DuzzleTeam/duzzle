8cb884bc0a632c975e0741ec1cf2efc2ab332c07
cho-hadam <s2019w16@e-mirim.hs.kr>
2021-06-22 05:59:54 +0900

    reset landing page

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -9,14 +9,9 @@ import Footer from "../Footer/Footer";
 import "../../../utils/Common.css";
 import "./Sections/LandingPage.css";
 
-// hooks
-import useScroll from "../../../hooks/useScroll";
-
 // 처음 웹페이지 접속시 보이는 화면
 // (chohadam, 2021-03-30)
 function LandingPage() {
-  const { y } = useScroll();
-
   return (
     <main className={"LandingPageContainer"}>
       {/* Duzzle Contents */}
