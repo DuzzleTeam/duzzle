cb326ea56010bed72455da5dbc02a18a5bb5f2cd
cho-hadam <s2019w16@e-mirim.hs.kr>
2021-06-24 01:19:21 +0900

    remove image

--- client/src/components/views/PostWritingPage/PostWritingPage.js
+++ client/src/components/views/PostWritingPage/PostWritingPage.js
@@ -181,6 +181,7 @@ function PostWritingPage() {
   const onChangeImages = (e) => {
     const { files } = e.target;
     setImages((prev) => [...prev, ...files]);
+    console.log(files);
 
     // preview images
     const previewImages = [];
@@ -306,6 +307,19 @@ function PostWritingPage() {
   // show image modal
   const [showImage, setShowImage] = useState("");
 
+  // remove image
+  const onRemoveImage = (index) => {
+    // remove preview
+    const removedPreview = previewImages.slice();
+    removedPreview.splice(index, 1);
+    setPreviewImages(removedPreview);
+
+    // remove input filelist
+    const removedImages = images.slice();
+    removedImages.splice(index, 1);
+    setImages(removedImages);
+  };
+
   // render
   return (
     <main className={"write-container__main"}>
@@ -390,7 +404,14 @@ function PostWritingPage() {
                   setShowImage(img);
                 }}
               >
-                <button className={"image-preview__button--cancel"}>
+                <button
+                  className={"image-preview__button--cancel"}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    onRemoveImage(index);
+                  }}
+                >
                   <img src="/images/image_cancel.png" alt="cancel" />
                 </button>
                 <img
@@ -402,6 +423,7 @@ function PostWritingPage() {
             ))}
         </section>
 
+        {/* 이미지 모달 */}
         {showImage !== "" && (
           <ShowImage src={showImage} setShowImage={setShowImage} />
         )}
