df699b8f53ba4a1e73186feb91ba9904bd96177b
dayeon <dayeon2399@naver.com>
2021-06-24 12:15:49 +0900

    이미지 미리보기 모달 밖 클릭 시 닫힘

--- client/src/components/Preview/ShowImage.js
+++ client/src/components/Preview/ShowImage.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useRef } from "react";
 
 import "./ShowImage.css";
+import useOutsideClick from "../views/NavBar/Sections/useOutsideClick";
 
 function ShowImage({ src, setShowImage }) {
+  // show-image__container 참조
+  const previewImagesContainer = useRef();
+  // 이미지 미리보기 컨테이너 밖 클릭 시 모달 접기
+  useOutsideClick(previewImagesContainer, () => {
+    setShowImage("");
+  });
+
   return (
     <div className={"show-image__container"}>
-      <div className="show-image__contents">
+      <div className="show-image__contents" ref={previewImagesContainer}>
         <button
           onClick={(e) => {
             e.preventDefault();
